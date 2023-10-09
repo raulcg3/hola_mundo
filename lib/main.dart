444aaeaae918c6c5928d29f61c8e5ce0684dd6d5
Raúl Calero <raulcalerogarcia2003@gmail.com>
2023-10-09 09:04:41 +0200

    Ejemplo de aplicacion de clase

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Ejemplo de aplicacion de clase',
       theme: ThemeData(
         // This is the theme of your application.
         //
